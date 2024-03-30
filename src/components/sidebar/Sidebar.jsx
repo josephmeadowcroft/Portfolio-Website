efc64b486867bdbe746d96cdd78131f573ef111e
josephmeadowcroft <149323761+josephmeadowcroft@users.noreply.github.com>
2024-03-30 10:31:25 +0000

    Fully animated sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,27 +4,27 @@ import { motion } from "framer-motion";
 import ToggleButton from "./toggleButton/ToggleButton";
 import Links from "./links/Links.jsx";
 
+const variants = {
+  open: {
+    clipPath: "circle(1200px at 50px 50px)",
+    transition: {
+      type: "spring",
+      stiffness: 20,
+    },
+  },
+  closed: {
+    clipPath: "circle(30px at 50px 50px)",
+    transition: {
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+      delay: 0.3,
+    },
+  },
+};
+
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
-
-  const variants = {
-    open: {
-      clipPath: "circle(1200px at 50px 50px)",
-      transition: {
-        type: "spring",
-        stiffness: 20,
-      },
-    },
-    closed: {
-      clipPath: "circle(30px at 50px 50px)",
-      transition: {
-        delay: 0.5,
-        type: "spring",
-        stiffness: 400,
-        damping: 40,
-      },
-    },
-  };
 
   return (
     <motion.div className="sidebar" animate={open ? "open" : "closed"}>

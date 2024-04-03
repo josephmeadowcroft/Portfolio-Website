e78c8774b2bc15cf44946885ae2710cdaaf8e899
josephmeadowcroft <149323761+josephmeadowcroft@users.noreply.github.com>
2024-04-03 21:28:32 +0100

    Added contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import './contact.scss'
+import React from "react";
+import "./contact.scss";
 
 const Contact = () => {
   return (
-    <div className="contact">Contact</div>
-  )
-}
+    <div className="contact">
+      <div className="textContainer">
+        <h1>Get in touch with me</h1>
+        <div className="item">
+          <h2>Email</h2>
+          <span>joseph.meadowcroft23@gmail.com</span>
+        </div>
+      </div>
+      <div className="formContainer">
+        <form>
+          <input type="text" required placeholder="Name" />
+          <input type="email" required placeholder="Email" />
+          <textarea rows={8} placeholder="Message" />
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
